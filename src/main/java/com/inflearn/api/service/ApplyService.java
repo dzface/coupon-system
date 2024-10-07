1cb9a4d26eafad02e48e12a56dda82efd36211d1
dzface <dzface031@gmail.com>
2024-10-07 20:02:05 +0900

    kafka 이벤트 처리 로직 추가

--- src/main/java/com/inflearn/api/service/ApplyService.java
+++ src/main/java/com/inflearn/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.inflearn.api.service;
 
 import com.inflearn.api.domain.Coupon;
+import com.inflearn.api.producer.CouponCreateProducer;
 import com.inflearn.api.repository.CouponCountRepository;
 import com.inflearn.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
-        this.couponRepository = couponRepository;
-        this.couponCountRepository = couponCountRepository;
-    }
+    private final CouponCreateProducer couponCreateProducer ;
+    public ApplyService(
+            CouponRepository couponRepository,
+            CouponCountRepository couponCountRepository,
+            CouponCreateProducer couponCreateProducer)
+                {
+                    this.couponRepository = couponRepository;
+                    this.couponCountRepository = couponCountRepository;
+                    this.couponCreateProducer = couponCreateProducer;
+                }
 
 
 
@@ -24,6 +31,6 @@ public class ApplyService {
         if(count > 100) {
             return;
         }
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
