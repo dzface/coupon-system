ed92e755897a01e2f4c6ed3bf2cbe2c6b1737459
dzface <dzface031@gmail.com>
2024-10-07 10:22:41 +0900

    1000명 요청시 오류발생 테스트 케이스 추가

--- src/test/java/com/inflearn/api/service/ApplyServiceTest.java
+++ src/test/java/com/inflearn/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +25,25 @@ class ApplyServiceTest {
         applyService.apply(1L);
         long count = couponRepository.count();
         assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch =new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(()-> {
+                try{
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        long count = couponRepository.count();
+        assertEquals(count, 100);
     }
 }
